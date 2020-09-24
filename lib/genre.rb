d426baf2a11cd2ad4c6c9c4d3b543d61e7c3d85a
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 20:43:21 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre 
+  
+  attr_accessor :name, :song, :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def songs 
+    Song.all.each {|song| song.genre == self}
+  end
+  
+  def artists 
+    songs.map {|song| song.artist}
+  end
+  
+  
+end
---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+end 
