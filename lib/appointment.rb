1f5c35eaf9d80838d6e41bfae7061b1b08ea09ef
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-24 20:41:41 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,8 +5,8 @@ class Appointment
   @@all = []
 
   def initialize(date, patient, doctor)
+    @date = date 
     @patient = patient
-    @date = date
     @doctor = doctor
     @@all << self
   end
